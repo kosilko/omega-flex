875080c899f9fdaf2d0bd79c3d1174c8fd2fd3a7
kosilka@gmail.com <kosilka@gmail.com>
2019-02-19 16:07:47 +0200

    optimize code

---
+++ index.js
@@ -0,0 +1,38 @@
+(function($) {
+'use strict';
+  $(document).ready(function() {
+    $('.omega-app-floater').resizable({
+      handles: 'w',
+      //minWidth: '30%',
+      resize: function(e, ui) {
+        var p = ui.element.closest('body');
+        var width = (100 / (parseInt(p.css('width')) / parseInt(ui.element.css('width')))).toFixed(2);
+        width = Math.min(95, width);
+        width = Math.max(30, width);
+        ui.element.css({
+          left: '', 
+          width: width + '%'
+        });
+      }
+    });
+    
+    
+    $('.file, .wave, .message, .main-content-item').each(function() {
+      var $e = $(this);
+      $e.css('cursor', 'pointer');
+      $e.click(function() {
+        var n = $('.' + $e.attr('class'), $e.parent()).length;
+        n++;
+        $('<span />').text(' #' + n).appendTo($e.clone().appendTo($e.parent()).click(function() {$e.click();}));
+        if ($e.is('.message')) {
+          $e.parent().scrollTop($e.parent()[0].scrollHeight);
+        }
+      });        
+    });
+    
+    
+    
+    
+
+  });
+})(jQuery);
